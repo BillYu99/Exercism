7bd0ee2ebb5fe035bab580e1d391bc23af979e90
BillYu99 <billyusfg@gmail.com>
2023-03-15 12:20:28 -0600

    Improved after seeing other solutions

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,61 +1,25 @@
+const MINS_DAY: i32 = 24 * 60;
+const MINS_HOUR: i32 = 60;
+
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: i32,
-    minutes: i32,
+    mins: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut newh = hours;
-        let mut newm = minutes;
-
-        if newm.abs() >= 60 {
-            newh = newh + newm / 60;
-            newm = newm % 60;
-        }
-        if newh.abs() >= 24 {
-            newh = newh % 24;
-        }
-        if newm < 0 {
-            newh = newh + newm / 60 - (newm % 60 != 0) as i32;
-            newm = 60 + newm;
-        }
-        if newh < 0 {
-            newh = 24 + newh;
-        }
-
         Self {
-            hours: newh,
-            minutes: newm,
+            mins: (((hours * MINS_HOUR + minutes) % MINS_DAY) + MINS_DAY) % MINS_DAY,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let mut newh = self.hours;
-        let mut newm = self.minutes + minutes;
-
-        if newm.abs() >= 60 {
-            newh = newh + newm / 60;
-            newm = newm % 60;
-        }
-        if newh.abs() >= 24 {
-            newh = newh % 24;
-        }
-        if newm < 0 {
-            newh = newh + newm / 60 - (newm % 60 != 0) as i32;
-            newm = 60 + newm;
-        }
-        if newh < 0 {
-            newh = 24 + newh;
-        }
-
         Self {
-            hours: newh,
-            minutes: newm,
+            mins: (((self.mins + minutes) % MINS_DAY) + MINS_DAY) % MINS_DAY,
         }
     }
 
     pub fn to_string(&self) -> String {
-        format!("{:02}", self.hours).to_string() + ":" + &format!("{:02}", self.minutes).to_string()
+        format!("{:02}:{:02}", self.mins / MINS_HOUR % 24, self.mins % 60)
     }
 }
