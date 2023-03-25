deedf49745beed56d894db35e001f35562cbc3a2
BillYu99 <billyusfg@gmail.com>
2023-03-25 16:42:35 -0600

    type ascription

---
+++ rust/armstrong-numbers/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let mut n = num;
+    let mut count = 0;
+    let mut arr = vec![];
+    let mut sum = 0;
+
+    while n > 0 {
+        let digit = n % 10;
+        arr.push(digit);
+        count += 1;
+        n = n / 10;
+    }
+
+    for i in arr {
+        sum = sum.checked_add(i.pow(count));
+    }
+
+    sum == num
+}
